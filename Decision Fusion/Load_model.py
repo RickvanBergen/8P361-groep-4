f4e25c2c285d4370aedcd989005a96c8c7631369
Rick van Bergen <r.j.p.v.bergen@student.tue.nl>
2020-03-12 16:24:03 +0100

    transfer learing model toegevoegd

--- Decision Fusion/Load_model.py
+++ Decision Fusion/Load_model.py
@@ -40,8 +40,13 @@ with open('models\model1.json', 'r') as f:
     model1 = model_from_json(f.read())
 with open('models\model2.json', 'r') as f:
     model2 = model_from_json(f.read())
+with open('models\\transfer_model.json', 'r') as f:
+    model3 = model_from_json(f.read())
 model1.load_weights('models\model1_weights.hdf5')
 model2.load_weights('models\model2_weights.hdf5')
+model3.load_weights('models\\transfer_model_weights.hdf5')
+
+
 
 train_gen, val_gen = get_pcam_generators(
     r"C:\Users\20174099\Documents\School\Jaar 3\Imaging Project\\")
@@ -53,11 +58,13 @@ val_gen.reset()
 pre_dict = dict()
 predict1 = model1.predict_generator(val_gen, steps=val_steps, verbose=1)
 pre_dict['predict1'] = predict1
-predict2 = model1.predict_generator(val_gen, steps=val_steps, verbose=1)
+predict2 = model2.predict_generator(val_gen, steps=val_steps, verbose=1)
 pre_dict['predict2'] = predict2
-pre_dict['predict_mean'] = np.mean([predict1,predict2],axis=0)
-pre_dict['predict_min'] = np.minimum(predict1,predict2)
-pre_dict['predict_max'] = np.maximum(predict1,predict2)
+predict_tran = model3.predict_generator(val_gen, steps=val_steps, verbose=1)
+pre_dict['predict_transfer'] = predict_tran
+pre_dict['predict_mean'] = np.mean([predict1,predict2,predict_tran],axis=0)
+pre_dict['predict_min'] = np.minimum(predict1,predict2,predict_tran)
+pre_dict['predict_max'] = np.maximum(predict1,predict2,predict_tran)
 
 for pr in pre_dict.keys():
     fpr, tpr, thresholds = roc_curve(true_labels, pre_dict[pr])
