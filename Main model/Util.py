f3d26ac096e16e394dec5b4cb28ba12c405bfe35
MyrtheVDB <54847320+MyrtheVDB@users.noreply.github.com>
2020-04-03 16:37:43 +0200

    added comments to Util.py

--- Main model/Util.py
+++ Main model/Util.py
@@ -2,7 +2,7 @@ import os
 from keras.models import model_from_json
 from keras.callbacks import ModelCheckpoint, TensorBoard
 
-
+# function to load the .json and _weights.hdf5 files of a pretrained model as input for another machine learning model
 def load_pr_model(name):
     path = os.path.join('Main model','models','Prediction_mod',name)
     with open(path + '.json', 'r') as f:
@@ -10,13 +10,7 @@ def load_pr_model(name):
     model.load_weights(path + '_weights.hdf5')
     return model
 
-def load_com_model(name):
-    path = os.path.join('Main model','models','Combination_mod',name)
-    with open(path + '.json', 'r') as f:
-        model = model_from_json(f.read())
-    model.load_weights(path + '_weights.hdf5')
-    return model
-
+# function save a combination model and create the logs
 def save_com_model(name,model):
     model_name = os.path.join('Main model','models','Combination_mod',name)
     model_filepath = model_name + '.json'
@@ -30,3 +24,12 @@ def save_com_model(name,model):
     tensorboard = TensorBoard(os.path.join('logs', model_name))
     callbacks_list = [checkpoint, tensorboard]
     return callbacks_list
+
+# function to load the .json and _weights.hdf5 files of a generated combination model 
+def load_com_model(name):
+    path = os.path.join('Main model','models','Combination_mod',name)
+    with open(path + '.json', 'r') as f:
+        model = model_from_json(f.read())
+    model.load_weights(path + '_weights.hdf5')
+    return model
+
